7eeaac572be5fcf8af0535dd409ebbe74a5bb312
Atul9119 <104543807+Atul9119@users.noreply.github.com>
2022-05-10 15:34:09 +0530

    Update TreeMapEx1.java

--- src/MapExamples/TreeMapEx1.java
+++ src/MapExamples/TreeMapEx1.java
@@ -5,6 +5,7 @@ import java.util.TreeMap;
 public class TreeMapEx1 {
 
 	public static void main(String[] args) {
+		System.out.println("I pull it");
 		TreeMap<Integer, String> tree1 = new TreeMap<Integer, String>();
 		tree1.put(2, "Bangalore");
 		tree1.put(5, "Mumbai Certral");
